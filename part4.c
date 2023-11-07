130082a2adae3110acb7c1497014eeccc552697f
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2023-11-07 12:13:13 -0800

    🛠

--- part4.c
+++ part4.c
@@ -4,6 +4,8 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 typedef struct {
     char** command_list;
@@ -80,17 +82,15 @@ void free_command_line(command_line* command)
 	free(command->command_list);
 }
 
-// #define N 3
 #define QUANTUM 2
 
-// int iters[N] = {8, 3, 13};
 int N = 0;
 int waiting = 0;
+int numRunning = 0;
 
 typedef struct Process {
     int pid;
     int status;
-    command_line *command;
 } Process;
 
 Process *processes = NULL;
@@ -110,6 +110,55 @@ void print_processes() {
     printf("--------------------------\n");
 }
 
+void processes_status_overview() {
+    printf("\n------- PROCESSES: -------\n\n");
+    for(int i = 0; i < N; i++) {
+        char *fpath;
+        asprintf(&fpath, "/proc/%d/status", processes[i].pid);
+
+        FILE *f = fopen(fpath, "r");
+
+        char *line = NULL;
+        size_t len = 128;
+        ssize_t read;
+
+        char *state = NULL;
+        int threads = 0;
+        int voluntary_ctxt_switches = 0;
+        int nonvoluntary_ctxt_switches = 0;
+
+        while((read = getline(&line, &len, f)) != -1) {
+            if(strncmp(line, "State:", 6) == 0) {
+                state = malloc(sizeof(char) * (strlen(line) + 1));
+                strcpy(state, line);
+            } else if(strncmp(line, "Threads:", 8) == 0) {
+                threads = atoi(line + 9);
+            } else if(strncmp(line, "voluntary_ctxt_switches:", 24) == 0) {
+                voluntary_ctxt_switches = atoi(line + 25);
+            } else if(strncmp(line, "nonvoluntary_ctxt_switches:", 27) == 0) {
+                nonvoluntary_ctxt_switches = atoi(line + 28);
+            }
+        }
+
+        printf("PROC (ID: %d) (STAT: ) | STATE: %s ", processes[i].pid, state);
+        printf("| THREADS: %d ", threads);
+        printf("| VOLUNTARY CTX SWITCHES: %d ", voluntary_ctxt_switches);
+        printf("| NON VOLUNTARY CTX SWITCHES: %d\n\n", nonvoluntary_ctxt_switches);
+
+        free(state);
+        free(line);
+
+        fclose(f);
+
+        free(fpath);
+        // printf("PROC (ID: %d) (STAT: ) | STATE: I (idle) ", processes[i].pid, processes[i].status);
+        // printf("| THREADS: 0 ");
+        // printf("| VOLUNTARY CTX SWITCHES: 0 ");
+        // printf("| NON VOLUNTARY CTX SWITCHES: 0\n\n");
+    }
+
+    printf("--------------------------\n");
+}
 
 void sigusr2_handler() {
     waiting++;
@@ -128,12 +177,25 @@ void sigalrm_handler() {
     pid_t child_pid = waitpid(processes[current_process].pid, &status, WNOHANG);
 
     if(child_pid != 0) {
+        // printf("Child PID != 0 | (%d)\n", child_pid);
+        // printf("Status: %d\n", status);
         if(WIFEXITED(status)) {
-            printf("(OS) >>> (ID: %d) Process Finished.", processes[current_process].pid);
+            printf("(OS) >>> (ID: %d) Process Finished.\n", processes[current_process].pid);
+            printf("(OS) >>> %d Processes Remaining.\n\n", numRunning);
             
             processes[current_process].status = 1;
+            numRunning--;
+            // printf("Num Running: %d\n", numRunning);
+            if(numRunning == 0) {
+                return;
+            }
         }
     }
+
+    /*************************************************/
+
+    //process overview
+    processes_status_overview();
 
     /*************************************************/
 
@@ -144,34 +206,29 @@ void sigalrm_handler() {
 
     //start next process
     printf("\n\n(OS) >>> (ID: %d) Process Resumed.\n\n", processes[current_process].pid);
+    // printf("\n(OS) >>> (ID: %d) (CMD: %s) Process Started.\n\n", processes[current_process].pid, processes[current_process].command[0]);
+
     kill(processes[current_process].pid, SIGCONT);
 
-    alarm(QUANTUM);
+    if (numRunning > 0) {
+        alarm(QUANTUM);
+    }
 }
 
 int main() {
     FILE *f = fopen("input.txt", "r");
 
-    char *line = NULL; size_t len = 0; ssize_t read;
+    size_t len = 128; char *line = malloc(sizeof(char) * len);  ssize_t read;
     
     for(; (read = getline(&line, &len, f)) != -1; N++);
+
+    free(line);
 
     fclose(f);
 
     processes = malloc(sizeof(Process) * N);
 
-    for(int i = 0; i < N; i++) {
-        processes[i].command = malloc(sizeof(command_line));
-    }
-
     f = fopen("input.txt", "r");
-
-    for(int i = 0; (getline(&line, &len, f) != -1); i++) {
-        command_line *cl = malloc(sizeof(command_line));
-        *cl = str_filler(line, " "); // fill command list
-        memcpy(processes[i].command, cl, sizeof(command_line));
-        free(cl);
-    }
 
 
     /************************************************/
@@ -191,24 +248,34 @@ int main() {
     /*************************************************/
 
     for(int i = 0; i < N; i++) {
+        size_t len = 128; char *line = malloc(sizeof(char) * len); 
+
+        getline(&line, &len, f);
+        command_line cl = str_filler(line, " ");
+
         processes[i].pid = fork();
 
         if(processes[i].pid == 0) {
-            printf("(PROC: %d) >>  Waiting...\n", getpid());
-
             kill(getppid(), SIGUSR2);
 
             if(sigwait(&set, &sig) == 0) {
-                if(execvp(processes[i].command->command_list[0], processes[i].command->command_list) != 0) {
-                    printf("Error executing command: %s\n", processes[i].command->command_list[0]);
+                if(execvp(cl.command_list[0], cl.command_list) != 0) {
+                    printf("Error executing command: %s\n", cl.command_list[0]);
                     exit(1);
                 }
+
             }
         } else if(processes[i].pid < 0) {
             printf("Error creating process %d\n", i);
             exit(1);
         }
+
+        free_command_line(&cl);
+        free(line);
     }
+
+    fclose(f);
+
     /************************************************/
 
     //setup all processes status
@@ -216,10 +283,13 @@ int main() {
         processes[i].status = 0;
     }
 
+    numRunning = N;
+
     /************************************************/
 
-    while(waiting < N);
+    // while(waiting < N);
 
+    // printf("\n(OS) >>> (ID: %d) (CMD: %s) Process Started.\n\n", processes[current_process].pid, processes[current_process].command[0]);
     printf("\n(OS) >>> (ID: %d) Process Started.\n\n", processes[current_process].pid);
 
     kill(processes[current_process].pid, SIGCONT);
@@ -228,15 +298,9 @@ int main() {
 
     /************************************************/
 
-    for(int i = 0; i < N; i++) {
-        waitpid(processes[i].pid, NULL, 0);
-    }
+    while(numRunning > 0);
 
     printf("\n\n(OS) >>> ALL PROCESSES FINISHED.\n\n");
-
-    for(int i = 0; i < N; i++) {
-        free_command_line(processes[i].command);
-    }
 
     free(processes);
 
